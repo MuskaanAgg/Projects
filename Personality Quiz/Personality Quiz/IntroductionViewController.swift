53dbc1f7a7b194f0ed31e78792b64c5ff6f82002
MuskaanAgg <muskaan0661.be23@chitkara.edu.in>
2025-09-16 10:43:51 +0530

    Personality quiz

---
+++ Personality Quiz/Personality Quiz/IntroductionViewController.swift
@@ -0,0 +1,16 @@
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func unwindToQuizInroduction(segue: UIStoryboardSegue) {
+        
+    }
+}
+
