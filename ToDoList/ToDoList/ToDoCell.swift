e541efd4da7c69b412adc799a1beda4e3d9b1936
MuskaanAgg <muskaan0661.be23@chitkara.edu.in>
2025-09-16 10:53:20 +0530

    To do list

---
+++ ToDoList/ToDoList/ToDoCell.swift
@@ -0,0 +1,31 @@
+
+
+import UIKit
+
+protocol ToDoCellDelegate: AnyObject {
+    func checkMarkTapped(sender: ToDoCell)
+}
+
+class ToDoCell: UITableViewCell {
+    
+    @IBOutlet weak var isCompleteButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    weak var delegate: ToDoCellDelegate?
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBAction func completeButtonTapped(_ sender: UIButton) {
+        delegate?.checkMarkTapped(sender: self)
+    }
+
+}
